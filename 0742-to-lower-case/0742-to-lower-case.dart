78ac4dc3df9e8518c199955d36349336d8d91c0f
hibabinth <hibabinth916@gmail.com>
2025-07-22 03:35:18 -0700

    Time: 0 ms (100%), Space: 145.4 MB (82.35%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.dart
@@ -0,0 +1,11 @@
+class Solution {
+  String toLowerCase(String s) {
+   return s.toLowerCase();
+}
+
+void main() {
+  String input = "Hello";
+  String output = toLowerCase(input);
+  print(output);  // Output: hello 
+  }
+}
