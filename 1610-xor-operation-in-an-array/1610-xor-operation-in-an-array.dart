73d37bfb8b3215b96ada23df6fed66e51af55d0f
hibabinth <hibabinth916@gmail.com>
2025-07-29 01:52:16 -0700

    Time: 0 ms (100%), Space: 150 MB (0%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.dart
@@ -0,0 +1,12 @@
+class Solution {
+  int xorOperation(int n, int start) {
+     int result = 0;
+
+    for (int i = 0; i < n; i++) {
+      int num = start + 2 * i;
+      result ^= num;
+    }
+
+    return result; 
+  }
+}
